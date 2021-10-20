b3ab3bb00d10b0c20bd014b7c88156b1cd6ab046
j-tran90 <johnt90@live.com>
2021-10-20 02:34:52 -0400

    fixed RemoteURI

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/list";
-exports.RemoteURI = process.env.RemoteURII;
-//export const RemoteURI = "mongodb+srv://john:2bRLK9GZX2jlaM0F@comp229-f2021.jlcqd.mongodb.net/list?retryWrites=true&w=majority";
-exports.HostName = (process.env.RemoteURII) ? "remotehost" : "localhost";
+exports.RemoteURI = process.env.RemoteURI;
+exports.HostName = (process.env.RemoteURI) ? "remotehost" : "localhost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
